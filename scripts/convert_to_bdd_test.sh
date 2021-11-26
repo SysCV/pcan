b36bb1809501f5e3df70ce144ef36577c5c9cd60
lkeab <keleiwhu@gmail.com>
2021-11-26 13:57:06 +0100

    update scripts

--- scripts/convert_to_bdd_test.sh
+++ scripts/convert_to_bdd_test.sh
@@ -1,4 +1,4 @@
 export PYTHONPATH=$PYTHONPATH:`pwd`
 
-python ./tools/to_bdd100k.py configs/segtrack-frcnn_r50_fpn_12e_bdd10k_fixed_refine_pcan.py --res eval_result_pcan_test.pkl --task seg_track --bdd-dir converted_results_test/ --nproc 2
+python ./tools/to_bdd100k.py configs/segtrack-frcnn_r50_fpn_12e_bdd10k_fixed_pcan.py --res eval_result_pcan_test.pkl --task seg_track --bdd-dir converted_results_test/ --nproc 2
 
--- scripts/train_pcan.sh
+++ scripts/train_pcan.sh
@@ -1,1 +1,1 @@
-bash ./tools/dist_train.sh configs/segtrack-frcnn_r50_fpn_12e_bdd10k_fixed_refine_pcan.py 4 --work-dir ./pcan_training_result/
+bash ./tools/dist_train.sh configs/segtrack-frcnn_r50_fpn_12e_bdd10k_fixed_pcan.py 4 --work-dir ./pcan_training_result/
